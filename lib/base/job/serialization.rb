cloudfoundry/vcap-services

Add file size and redirect limitation to import_form_url job
Change-Id: Ic50cd33d2742ea7039e21b093152c7c9685b0bee

base/lib/base/job/serialization.rb
@@ -241,11 +241,30 @@ module VCAP::Services::Base::AsyncJob
 
       # Fetch remote uri and stream content to file.
       def fetch_url(url, file_path)
-        # TODO check the file size before download?
+        max_download_size = (@config["serialization"] && @config["serialization"]["max_download_size_mb"] || 10).to_i * 1024 * 1024 # 10M by default
+        max_redirects = @config["serialization"] && @config["serialization"]["max_download_redirects"] || 5
+
         File.open(file_path, "wb+") do |f|
           c = Curl::Easy.new(url)
+          # auto redirect
+          c.follow_location = true
+          c.max_redirects = max_redirects
+
+          c.on_header do |header|
+            k,v = header.split(/ *: */)
+            if k.downcase == 'content-length' && v.to_i > max_download_size
+              raise ServiceError.new(ServiceError::FILESIZE_TOO_LARGE, url, v.strip, max_download_size)
+            end
+
+            header.size
+          end
+
           c.on_body{|data| f.write(data)}
-          c.perform
+          begin
+            c.perform
+          rescue Curl::Err::TooManyRedirectsError
+            raise ServiceError.new(ServiceError::TOO_MUCH_REDIRECTS, url, max_redirects)
+          end
         end
       end
 
@@ -276,7 +295,7 @@ module VCAP::Services::Base::AsyncJob
             end
 
             raise "Can't find temp file: #{@temp_file_path}" unless File.exists? temp_file_path
-            raise ServiceError.new(SerivceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
+            raise ServiceError.new(ServiceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
 
             @snapshot_id = new_snapshot_id
             @snapshot_path = get_dump_path(name, snapshot_id)

base/lib/base/service_error.rb
@@ -55,6 +55,8 @@ module VCAP
           JOB_QUEUE_TIMEOUT = [30801, HTTP_INTERNAL, "Job timeout after waiting for %s seconds."]
           JOB_TIMEOUT = [30802, HTTP_INTERNAL, "Job is killed since it runs longer than ttl: %s seconds."]
           BAD_SERIALIZED_DATAFILE = [30803, HTTP_INTERNAL, "Invalid serialized data file from: %s"]
+          FILESIZE_TOO_LARGE = [30804, HTTP_BAD_REQUEST, "Size of file from url %s is %s, max allowed %s"]
+          TOO_MUCH_REDIRECTS = [30805, HTTP_BAD_REQUEST, "Too much redirects from url:%s, max redirects allowed is %s"]
 
           # 31000 - 32000  Service-specific Error
           # Defined in services directory, for example mongodb/lib/mongodb_service/

cloudfoundry-attic/vcap-services-base

lib/base/job/serialization.rb
@@ -241,11 +241,38 @@ module VCAP::Services::Base::AsyncJob
 
       # Fetch remote uri and stream content to file.
       def fetch_url(url, file_path)
-        # TODO check the file size before download?
+        max_download_size = (@config["serialization"] && @config["serialization"]["max_download_size_mb"] || 10).to_i * 1024 * 1024 # 10M by default
+        max_redirects = @config["serialization"] && @config["serialization"]["max_download_redirects"] || 5
+
         File.open(file_path, "wb+") do |f|
           c = Curl::Easy.new(url)
-          c.on_body{|data| f.write(data)}
-          c.perform
+          # auto redirect
+          c.follow_location = true
+          c.max_redirects = max_redirects
+
+          c.on_header do |header|
+            if c.downloaded_content_length > max_download_size
+              raise ServiceError.new(ServiceError::FILESIZE_TOO_LARGE, url, c.downloaded_content_length, max_download_size)
+            end
+
+            header.size
+          end
+
+          bytes_downloaded = 0
+          c.on_body do |data|
+            # calculate bytes downloaded for chucked response
+            bytes_downloaded += data.size
+            if bytes_downloaded > max_download_size
+              raise ServiceError.new(ServiceError::FILESIZE_TOO_LARGE, url, bytes_downloaded, max_download_size)
+            end
+            f.write(data)
+          end
+
+          begin
+            c.perform
+          rescue Curl::Err::TooManyRedirectsError
+            raise ServiceError.new(ServiceError::TOO_MANY_REDIRECTS, url, max_redirects)
+          end
         end
       end
 
@@ -276,7 +303,7 @@ module VCAP::Services::Base::AsyncJob
             end
 
             raise "Can't find temp file: #{@temp_file_path}" unless File.exists? temp_file_path
-            raise ServiceError.new(SerivceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
+            raise ServiceError.new(ServiceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
 
             @snapshot_id = new_snapshot_id
             @snapshot_path = get_dump_path(name, snapshot_id)

lib/base/service_error.rb
@@ -55,6 +55,8 @@ module VCAP
           JOB_QUEUE_TIMEOUT = [30801, HTTP_INTERNAL, "Job timeout after waiting for %s seconds."]
           JOB_TIMEOUT = [30802, HTTP_INTERNAL, "Job is killed since it runs longer than ttl: %s seconds."]
           BAD_SERIALIZED_DATAFILE = [30803, HTTP_INTERNAL, "Invalid serialized data file from: %s"]
+          FILESIZE_TOO_LARGE = [30804, HTTP_BAD_REQUEST, "Size of file from url %s is %s, max allowed %s"]
+          TOO_MANY_REDIRECTS = [30805, HTTP_BAD_REQUEST, "Too many redirects from url:%s, max redirects allowed is %s"]
 
           # 31000 - 32000  Service-specific Error
           # Defined in services directory, for example mongodb/lib/mongodb_service/