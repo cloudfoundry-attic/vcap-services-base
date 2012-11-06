082e0d4514ee2d72dfc8ca38fe8bb01a9564f84e
Tang Rui <rtang@vmware.com>
2012-11-05 22:52:43 -0700

    Use "sh" to remove directories in unprovision
    Change-Id: I79ec27e40b67a125a205db528d12980b61742758

--- lib/base/utils.rb
+++ lib/base/utils.rb
@@ -123,34 +123,47 @@ module VCAP::Services::Base::Utils
   module ClassMethods
     def sh(*args)
       options = args[-1].respond_to?(:to_hash) ? args.pop.to_hash: {}
-      options = { :timeout => 5.0, :max => 1024 * 1024, :sudo => true }.merge(options)
-      arg = options[:sudo]==false ? args[0] : "sudo " << args[0]
+      options = { :timeout => 5.0, :max => 1024 * 1024, :sudo => true, :block => true }.merge(options)
+      arg = options[:sudo] == false ? args[0] : "sudo " << args[0]
 
       begin
         stdin, stdout, stderr, status = Open3.popen3(arg)
         pid = status[:pid]
-        start = Time.now
-
-        out_buf=""
-        err_buf=""
-        # manually ping the process per second to check whether the process is alive or not
-        while (Time.now-start)<options[:timeout] && status.alive?
-          begin
-            out_buf << stdout.read_nonblock(4096)
-            err_buf << stderr.read_nonblock(4096)
-          rescue IO::WaitReadable, EOFError
+        out_buf = ""
+        err_buf = ""
+        if options[:block]
+          start = Time.now
+          # Manually ping the process per second to check whether the process is alive or not
+          while (Time.now - start) < options[:timeout] && status.alive?
+            begin
+              out_buf << stdout.read_nonblock(4096)
+              err_buf << stderr.read_nonblock(4096)
+            rescue IO::WaitReadable, EOFError
+            end
+            sleep 0.2
           end
-          sleep 0.2
-        end
 
-        if status.alive?
-          Process.kill("TERM", pid)
-          Process.detach(pid)
-          raise RuntimeError, "sh #{args} executed with failure and process with pid #{pid} timed out:\nstdout:\n#{out_buf}\nstderr:\n#{err_buf}"
+          if status.alive?
+            Process.kill("TERM", pid)
+            Process.detach(pid)
+            raise RuntimeError, "sh #{args} executed with failure and process with pid #{pid} timed out:\nstdout:\n#{out_buf}\nstderr:\n#{err_buf}"
+          end
+          exit_status = status.value.exitstatus
+          raise RuntimeError, "sh #{args} executed with failure and process with pid #{pid} exited with #{status.value.exitstatus}:\nstdout:\n#{out_buf}\nstderr:\n#{err_buf}" unless exit_status == 0
+          exit_status
+        else
+          # If the work is still not done after timeout, then kill the process and record an erorr log
+          EM.add_timer(options[:timeout]) do
+            if status.alive?
+              Process.kill("TERM", pid)
+              Process.detach(pid)
+              logger.error "sh #{args} executed with pid #{pid} timed out" if logger
+            else
+              logger.error "sh #{args} executed with failure, the exit status is #{status.value.exitstatus}" if status.value.exitstatus != 0 && logger
+            end
+          end
+          return 0
         end
-        exit_status = status.value.exitstatus
-        raise RuntimeError, "sh #{args} executed with failure and process with pid #{pid} exited with #{status.value.exitstatus}:\nstdout:\n#{out_buf}\nstderr:\n#{err_buf}" unless exit_status == 0
-        exit_status
       rescue Errno::EPERM
         raise RuntimeError, "sh #{args} executed with failure and process with pid #{pid} cannot be killed (privilege issue?):\nstdout:\n#{out_buf}\nstderr:\n#{err_buf}"
       rescue Errno::ESRCH
