e4c41d9e83c990a93395a7ae53b10826aa71765d
Ketan Deshpande and Michael Maximilien <maxim@us.ibm.com>
2015-01-07 16:36:07 -0800

    Fixed for newer version of rspec

--- spec/unit_test/service_plan_change_set_spec.rb
+++ spec/unit_test/service_plan_change_set_spec.rb
@@ -3,19 +3,19 @@ require 'base/service_plan_change_set'
 
 module VCAP::Services
   describe ServicePlanChangeSet do
-    let(:service) { stub }
+    let(:service) { double() }
     subject { described_class.new(service, "my_guid") }
 
     it 'has a list of plans to add' do
-      subject.plans_to_add.should == []
+      expect(subject.plans_to_add).to eq([])
     end
 
     it 'has a list of plans to update' do
-      subject.plans_to_update.should == []
+      expect(subject.plans_to_update).to eq([])
     end
 
     it 'has a service' do
-      subject.service.should == service
+      expect(subject.service).to eq(service)
     end
   end
 end
